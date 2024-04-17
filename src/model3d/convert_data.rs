d1a022e5e06cde8e37cbca5d4d2cb5d0ae579827
Geneus003 <geneus003@gmail.com>
2024-04-17 23:19:07 +0300

    [WIP] converting model3d to model2d

--- src/model3d/convert_data.rs
+++ src/model3d/convert_data.rs
@@ -59,6 +59,43 @@ impl Model3D {
         if cords_x.len() != cords_y.len() {
             return Err("Vectors cords_x and cords_y must be with same size")
         }
+
+        let source_model = &self.model;
+        let source_model_mask = &self.model_mask;
+        let source_model_size_y = self.borders[0].len();
+        let source_model_size_x = self.borders[0][0].len();
+        let source_model_size_z = self.borders.len();
+
+        let model_ex: bool = !self.model.is_empty();
+        let mask_ex: bool = !self.model_mask.is_empty();
+
+        let mut model: Vec<Vec<i32>> = Vec::with_capacity(if model_ex {cords_x.len()} else {0});
+        let mut model_mask: Vec<Vec<u8>> = Vec::with_capacity(if mask_ex {cords_x.len()} else {0});
+
+        for (num, x_cord) in cords_x.iter().enumerate() {
+            let y_cord = cords_y[num];
+
+            if y_cord >= source_model_size_y || *x_cord >= source_model_size_x {
+                return Err("Invalid coodinates: it must be smaller than model size")
+            }
+
+            if model_ex{
+                let mut temp_vec: Vec<i32> = Vec::with_capacity(source_model_size_z);
+                for depth in source_model {
+                    temp_vec.push(depth[y_cord][*x_cord]);
+                }
+                model.push(temp_vec);
+            }
+
+            if mask_ex {
+                let mut temp_vec: Vec<u8> = Vec::with_capacity(source_model_size_z);
+                for depth in source_model_mask {
+                    temp_vec.push(depth[y_cord][*x_cord]);
+                }
+                model_mask.push(temp_vec);
+            }
+
+        }
         Err("Literally nothing")
     }
 }
